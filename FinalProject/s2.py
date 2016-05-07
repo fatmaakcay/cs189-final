6471b2797eede7d93503c7c9e134d046c50c6a9e
emilysw <emilyswang@college.harvard.edu>
2016-05-07 16:06:30 -0400

    a

--- FinalProject/s2.py
+++ FinalProject/s2.py
@@ -9,12 +9,24 @@ import speech_recognition as sr
 index = pyaudio.PyAudio().get_device_count() - 1
 print index
 
+#!/usr/bin/env python3
+
+# NOTE: this example requires PyAudio because it uses the Microphone class
+
+
+# obtain audio from the microphone
 r = sr.Recognizer()
-for i in range(5):
-    with sr.Microphone() as source:
+with sr.Microphone() as source:
+    print("Say something!")
     audio = r.listen(source)
 
+# recognize speech using Google Speech Recognition
 try:
-    print("You said " + r.recognize(audio))
-except LookupError:
-    print("Could not understand audio")
+# for testing purposes, we're just using the default API key
+# to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
+# instead of `r.recognize_google(audio)`
+    print("Google Speech Recognition thinks you said " + r.recognize_google(audio))
+except sr.UnknownValueError:
+    print("Google Speech Recognition could not understand audio")
+except sr.RequestError as e:
+    print("Could not request results from Google Speech Recognition service; {0}".format(e))
