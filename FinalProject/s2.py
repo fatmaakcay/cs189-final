da75fedb92d2bcb64899ba3fc1078fb5c4e988a6
emilysw <emilyswang@college.harvard.edu>
2016-05-07 16:08:39 -0400

    a

--- FinalProject/s2.py
+++ FinalProject/s2.py
@@ -25,6 +25,7 @@ try:
 # for testing purposes, we're just using the default API key
 # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
 # instead of `r.recognize_google(audio)`
+    print("detected voices")
     print("Google Speech Recognition thinks you said " + r.recognize_google(audio))
 except sr.UnknownValueError:
     print("Google Speech Recognition could not understand audio")
